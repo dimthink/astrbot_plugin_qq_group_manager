1879080f258f62696786f00ce4dfc589c442c563
fsptd <1336065617@qq.com>
2026-09-13 17:57:32 +0800

    docs: v0.4.1 版本号、权重说明与更新日志

--- main.py
+++ main.py
@@ -98,7 +98,7 @@ from .src.utils import (
 from .src.web_api import EventBus, WebApi
 
 PLUGIN_NAME = "astrbot_plugin_qq_group_manager"
-VERSION = "0.4.0"
+VERSION = "0.4.1"
 
 STATE_FLUSH_INTERVAL = 30.0
 MAINTENANCE_INTERVAL = 3600.0
